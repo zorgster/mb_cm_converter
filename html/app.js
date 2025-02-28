9ae87021c5838c2725fae3b9f2fe8d1d2ff1ccbb
Oliver Slay <mail@oliverslay.com>
2025-02-28 15:29:19 +0000

    remove .js from app.js ln 18

--- html/app.js
+++ html/app.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const parent = document.getElementById('parent').value;
 
         try {
-            const response = await fetch('/functions/calculate-cm.js', {
+            const response = await fetch('/functions/calculate-cm', {
                 method: 'POST',
                 body: JSON.stringify({ chromosome, start, end, parent }),
             });
