425bb3270aeeff4e2743b7fed37c62d9c7daa0c7
Oliver Slay <mail@oliverslay.com>
2025-02-28 15:26:42 +0000

    remove .netlify folder from app.js

--- html/app.js
+++ html/app.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const parent = document.getElementById('parent').value;
 
         try {
-            const response = await fetch('/.netlify/functions/calculate-cm.js', {
+            const response = await fetch('/functions/calculate-cm.js', {
                 method: 'POST',
                 body: JSON.stringify({ chromosome, start, end, parent }),
             });
