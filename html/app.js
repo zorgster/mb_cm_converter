8df82bac04ed0406fd95c4deb13b816162ae253a
Oliver Slay <mail@oliverslay.com>
2025-02-28 15:23:28 +0000

    add .js to calculate-cm

--- html/app.js
+++ html/app.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const parent = document.getElementById('parent').value;
 
         try {
-            const response = await fetch('/.netlify/functions/calculate-cm', {
+            const response = await fetch('/.netlify/functions/calculate-cm.js', {
                 method: 'POST',
                 body: JSON.stringify({ chromosome, start, end, parent }),
             });
